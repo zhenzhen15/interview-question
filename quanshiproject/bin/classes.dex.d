b99a5f724fe4b345dc3d63da639c2c2854be2f5c
zhenzhen15 <janyzhen15@163.com>
2017-07-20 12:58:07 +0800

    pom.xml

---
+++ quanshiproject/bin/classes.dex.d
@@ -0,0 +1,21 @@
+C:\work\interview-question\quanshiproject\bin\classes.dex : \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\cases\ChooseEnviromentCase.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\cases\HomePgeCase.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\cases\LoginAppCase.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\cases\QuanShi.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\publics\FindAssistant.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\publics\StartApp.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\task\HomePageLogin.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\task\LoginApp.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\task\SettingEnviroment.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\test\AutoTest.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\test\ChooseEnviromentCaseTest.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\test\HomePageCaseTest.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\test\LoginAppCaseTest.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\tool\UiAutomatorTool.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\ui\constant\EnviromentPageContant.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\ui\constant\HomePageContant.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\ui\constant\LoginPageContant.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\ui\page\EnviromentPage.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\ui\page\HomePage.class \
+C:\work\interview-question\quanshiproject\bin\classes\meeting\ui\page\LoginPage.class \
